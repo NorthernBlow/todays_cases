b87ea27b703f795ac0ef4f756580f41b13275419
NorthernBlow <rheinhart@yandex.ru>
2022-11-22 19:55:07 +0300

    so it begins

---
+++ todoapp.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int add() {
+
+}
+
+
+int del() {
+
+}
+
+
+int done() {
+
+
+}
+
+
+int ls() {
+
+
+}
+
+
+int help() {
+
+
+}
+
+
+int report() {
+
+
+}
+
+
+int main() {
+
+
+}
