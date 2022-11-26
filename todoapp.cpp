755ff88e9d4db7f5e807816eeb35fd0156666ca7
NorthernBlow <rheinhart@yandex.ru>
2022-11-26 13:45:41 +0300

    create main func

--- todoapp.cpp
+++ todoapp.cpp
@@ -1,5 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
+
+
+struct Sample {   // создан новый тип данных Sample
+
+	int a;
+	int b;
+	string m;
+
+
+};
+
+Sample return_pair(int x, string field) {    // функция типа Sample
+
+	Sample result;  // локальный экземпляр структуры
+
+	result.a = x * x;	// заполняю нужными значениями
+	result.b = x * x * x;
+	result.m = field
+
+	return result;
+
+
+}
 
 
 int add() {
@@ -35,8 +59,36 @@ int report() {
 
 }
 
+class TodoItem {
+private:
+	int id;
+	std::string description;
+	bool completed;
+public:
+
+	TodoItem() : id(0), description(""), completed(false) {}
+	~TodoItem() = default;
+
+};
+
 
 int main() {
 
+	int x;
+	string field;
+	std::list<TodoItem> todoItems;
+
+	todoItems.clear();
+	while (1) {
+		system("sls");
+		std::cout << "Todo list Maker - " << version << std::endll;
+	}
+
+	cin >> x >> field;
+	Sample pairs = return_pair(x, field);
+	std::cout << pairs.a << ' ' << pairs.b << pairs.m << '\n'; 
+
+std::string version = "v.0.1.0";
+std::cout << "Todo list maker - " << version << std::endl;
 
 }
